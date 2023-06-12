ae2576d29bad003f19f70ebb27cd32529aec4924
elisa-morales <morales.elisa@yahoo.it>
2023-06-12 19:39:21 +0200

    fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = import.meta.env.VITE_API_KEY
 
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    axios
+      .get(url)
+      .then((res) => {
+        const datini = res.data.results
+        console.log(datini)
+      })
+      .catch((error) => console.log(`Error: ${error}`))
+  }
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!!!</h1>
+      <h1 className="text-3xl font-bold underline">la mia app funzionaa</h1>
     </>
   )
 }
