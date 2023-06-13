34ec353538a54dc432cd7785f5b65deb34f575f1
elisa-morales <morales.elisa@yahoo.it>
2023-06-13 10:47:40 +0200

    quick re-org

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useEffect } from "react"
-import axios from "axios"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+//import axios from "axios"
+
+function Home() {
+  return <h1>ciao</h1>
+}
 
 function App() {
-  const url = import.meta.env.VITE_API_KEY
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const fetchData = () => {
-    axios
-      .get(url)
-      .then((res) => {
-        const datini = res.data.results
-        console.log(datini)
-      })
-      .catch((error) => console.log(`Error: ${error}`))
-  }
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">la mia app funzionaa</h1>
-    </>
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
