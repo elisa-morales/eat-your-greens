60731180ef466feb4e42ddf74abe1dbc7861e13a
elisa-morales <morales.elisa@yahoo.it>
2023-06-16 14:27:25 +0200

    fixed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,22 @@
 export default function Footer() {
   return (
     <>
-      <footer className="">
-        <div className="flex gap-3 py-3.5 px-4">
-          <p className="text-xs pt-1"> &#169; Elisa Morales</p>
-          <p>―</p>
+      <footer className="flex gap-3 py-3.5 px-4">
+        <p className="text-xs pt-1"> &#169; Elisa Morales</p>
+        <p>―</p>
 
-          <div className="flex gap-6 text-primary text-lg">
-            <a href="https://www.linkedin.com/in/elisa-morales-dev/" target="_blank">
-              <i className="hover:text-hoverColor bx bxl-linkedin"></i>
-            </a>
+        <div className="flex gap-6 text-primary text-lg">
+          <a href="https://www.linkedin.com/in/elisa-morales-dev/" target="_blank">
+            <i className="hover:text-hoverColor bx bxl-linkedin"></i>
+          </a>
 
-            <a href="https://github.com/elisa-morales" target="_blank">
-              <i className="hover:text-hoverColor bx bxl-github"></i>
-            </a>
+          <a href="https://github.com/elisa-morales" target="_blank">
+            <i className="hover:text-hoverColor bx bxl-github"></i>
+          </a>
 
-            <a href="https://talent.start2impact.it/profile/elisa-morales" target="_blank">
-              <i className="hover:text-hoverColor bx bx-rocket"></i>
-            </a>
-          </div>
+          <a href="https://talent.start2impact.it/profile/elisa-morales" target="_blank">
+            <i className="hover:text-hoverColor bx bx-rocket"></i>
+          </a>
         </div>
       </footer>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <header className="top-0 px-4 py-3.5 text-2xl z-1 shadow-sm">
+      <header className="px-4 py-3.5 text-2xl z-1 shadow-sm">
         <div className="mb-6 text-primary">
           <i className="absolute top-3 left-4 bx bx-home-alt  hover:text-hoverColor cursor-pointer"></i>
           <i className="absolute top-3 right-4 bx bx-bookmark-heart  hover:text-hoverColor cursor-pointer"></i>
