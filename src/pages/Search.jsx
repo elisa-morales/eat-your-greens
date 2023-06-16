580caab296f0c7e45bbbb93644a4a1b3d9613e23
elisa-morales <morales.elisa@yahoo.it>
2023-06-16 11:07:33 +0200

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -20,18 +20,19 @@ export default function Search() {
   }
 
   const recipeElements = recipes.map((recipe) => (
-    <div key={recipe.id} className="flex flex-col justify-center p-3 pb-6 bg-white rounded-md shadow-sm">
-      <img src={recipe.image} />
+    <div key={recipe.id} className="relative flex flex-col justify-center rounded-md shadow-sm">
+      <img className="brightness-50 rounded-md" src={recipe.image} />
       <div className="">
-        <h3 className="">{recipe.title}</h3>
+        <h2 className="absolute bottom-5 left-5 pr-5 font-semibold text-white shadow-sm">{recipe.title}</h2>
       </div>
     </div>
   ))
 
   return (
-    <div className="m-10">
-      <h1>Blabl</h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">{recipeElements}</div>
+    <div className="m-5">
+      <h1 className="text-primary font-bold">Search results for "egg"</h1>
+      <h2 className="mb-6">{recipes.length} results</h2>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">{recipeElements}</div>
     </div>
   )
 }
