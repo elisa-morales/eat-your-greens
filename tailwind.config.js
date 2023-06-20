35726794f34bdd40ef3f9bf88ebbe52ec52e8f1a
elisa-morales <morales.elisa@yahoo.it>
2023-06-20 18:49:27 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const plugin = require("tailwindcss/plugin")
 
 export default {
-  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
       textColor: "#020302",
